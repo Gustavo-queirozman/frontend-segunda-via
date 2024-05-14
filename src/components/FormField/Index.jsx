0bb5fc7e47cccffd85667003c68c7baa7b32ab0e
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-14 13:36:56 -0300

    Recurso: Cria componente de campo do formulário

---
+++ src/components/FormField/Index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function FormField(props) {
+    <div className="mt-8 mx-24">
+        <label className="text-cinza text-sm">{props.label}:</label>
+        <input
+            type={props.type}
+            name={props.name}
+            placeholder={props.placeholder}
+            className="px-1 appearance-none block min-w-full py-4 leading-tight text-cinza bg-none outline-none border-b-2 border-cinza-70 hover:border-cinza delay-75"
+            onChange={props.handleChange}
+            value={props.value}
+        />
+    </div>
+}
+
+export default FormField;
