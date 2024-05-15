d025b739097037e40f5cce109436dc6ffe477142
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-15 09:55:57 -0300

    Refatoraçao: Refatora funcionalidade de cadastrar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div class="App">
-        <Entrar/>
+        <Cadastrar/>
     </div>
   )
 }
--- src/pages/Cadastrar/Index.jsx
+++ src/pages/Cadastrar/Index.jsx
@@ -3,42 +3,65 @@ import FormField from "../../components/FormField/Index";
 
 function Cadastrar(props) {
     return (
-        <div>
-            <FormField
-                label="Cnp"
-                type="cnp"
-                name="cnp"
-                placeholder=""
-                onChange=""
-                value=""
-            />
+        <div className="min-h-screen  bg-white flex ">
+            <div className="flex-1 p-4">
+                <div className="mx-auto w-full max-w-sm">
+                    <div className="flex justify-center h-auto mx-auto ">
+                        <img src="Logo.png" alt="" className="h-28 mt-14 bottom-10" />
+                    </div>
+                </div>
 
-            <FormField
-                label="Email"
-                type="email"
-                name="email"
-                placeholder=""
-                onChange=""
-                value=""
-            />
+                <div className="mt-2">
+                    <form>
+                        <FormField
+                            label="Cnp"
+                            type="cnp"
+                            name="cnp"
+                            placeholder=""
+                            onChange=""
+                            value=""
+                        />
 
-            <FormField
-                label="Senha"
-                type="password"
-                name="password"
-                placeholder=""
-                onChange=""
-                value=""
-            />
+                        <FormField
+                            label="Email"
+                            type="email"
+                            name="email"
+                            placeholder=""
+                            onChange=""
+                            value=""
+                        />
 
-            <FormField
-                label="Confirmar senha"
-                type="text"
-                name="c_password"
-                placeholder=""
-                onChange=""
-                value=""
-            />
+                        <FormField
+                            label="Senha"
+                            type="password"
+                            name="password"
+                            placeholder=""
+                            onChange=""
+                            value=""
+                        />
+
+                        <FormField
+                            label="Confirmar senha"
+                            type="text"
+                            name="c_password"
+                            placeholder=""
+                            onChange=""
+                            value=""
+                        />
+
+                        <div className="mt-11 mx-24">
+                            <button className="bg-secundaria text-white px-3 appearance-none block min-w-full py-4 leading-tight rounded-full transition ease-in-out delay-150 bg-secundaria-500 hover:-translate-y-1 hover:scale-100 hover:bg-primaria duration-300"
+                                onClick={(e) => handleSubmit(e)}>
+                                Entrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div
+                className="hidden sm:block relative flex-1 p-4 bg-cover bg-[url('capa.jpg')]"
+            ></div>
         </div>
     );
 }
