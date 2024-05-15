4cb8a54fe65df9e83d0231677d3d147aa9f6f94d
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-15 10:20:06 -0300

    Recurso: Cria funcionalidade esquici minha senha

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Entrar from './pages/Entrar/Index'
 import Cadastrar from './pages/Cadastrar/Index'
+import EsqueciMinhaSenha from './pages/EsqueciMinhaSenha/Index'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div class="App">
-        <Cadastrar/>
+        <EsqueciMinhaSenha/>
     </div>
   )
 }
