c5d8566329956b7bb36f40cef07ecb53be931a5a
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-17 15:12:53 -0300

    Correção: Corrigi formulário

--- src/pages/ModificarSenha/Index.jsx
+++ src/pages/ModificarSenha/Index.jsx
@@ -1,31 +1,40 @@
 import React from "react";
-import FormField from "../../components/FormField/Index";
 import Header from "../../components/Header/Index";
+
 function ModificarSenha() {
     return (
         <div className="min-h-screen  bg-white flex ">
             <div className="flex-1 p-4">
-                <Header/>
-                
+                <Header />
+
                 <div className="mt-2">
                     <form>
-                        <FormField
-                            label="Senha"
-                            type="password"
-                            name="password"
-                            placeholder=""
-                            onChange=""
-                            value=""
-                        />
 
-                        <FormField
-                            label="Confirmar senha"
-                            type="c_password"
-                            name="c_password"
-                            placeholder=""
-                            onChange=""
-                            value=""
-                        />
+                        <div className="mt-8 mx-24">
+                            <label className="text-cinza text-sm">Senha:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                placeholder=""
+                                className="px-1 appearance-none block min-w-full py-4 leading-tight text-cinza bg-none outline-none border-b-2 border-cinza-70 hover:border-cinza delay-75"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                            />
+                        </div>
+
+                        <div className="mt-8 mx-24">
+                            <label className="text-cinza text-sm">Confirmar senha:</label>
+                            <input
+                                type="password"
+                                name="c_password"
+                                placeholder=""
+                                className="px-1 appearance-none block min-w-full py-4 leading-tight text-cinza bg-none outline-none border-b-2 border-cinza-70 hover:border-cinza delay-75"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+
+                            />
+                        </div>
+
 
                         <div className="mt-11 mx-24">
                             <button className="bg-secundaria text-white px-3 appearance-none block min-w-full py-4 leading-tight rounded-full transition ease-in-out delay-150 bg-secundaria-500 hover:-translate-y-1 hover:scale-100 hover:bg-primaria duration-300"
