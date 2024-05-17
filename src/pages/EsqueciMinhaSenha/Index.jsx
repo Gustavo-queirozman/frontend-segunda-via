e3a32e32c5429c362947c0dd0ef6fc4dd83d2ef3
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-17 15:08:07 -0300

    Correção: Corrigi funcionalidade de consumir api

--- src/pages/EsqueciMinhaSenha/Index.jsx
+++ src/pages/EsqueciMinhaSenha/Index.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-import FormField from "../../components/FormField/Index";
 import Header from "../../components/Header/Index";
-function EsqueciMinhaSenha(){
+
+function EsqueciMinhaSenha() {
     return (
         <div className="min-h-screen  bg-white flex ">
-        <div className="flex-1 p-4">
-            <Header/>
+            <div className="flex-1 p-4">
+                <Header />
 
-            <div className="mt-2">
-                <form>
-                    <FormField
-                        label="Email"
-                        type="email"
-                        name="email"
-                        placeholder=""
-                        onChange=""
-                        value=""
-                    />
+                <div className="mt-2">
+                    <form>
+
+                        <div className="mt-8 mx-24">
+                            <label className="text-cinza text-sm">Email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder=""
+                                className="px-1 appearance-none block min-w-full py-4 leading-tight text-cinza bg-none outline-none border-b-2 border-cinza-70 hover:border-cinza delay-75"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+
+                            />
+                        </div>
 
 
-                    <div className="mt-11 mx-24">
-                        <button className="bg-secundaria text-white px-3 appearance-none block min-w-full py-4 leading-tight rounded-full transition ease-in-out delay-150 bg-secundaria-500 hover:-translate-y-1 hover:scale-100 hover:bg-primaria duration-300"
-                            onClick={(e) => handleSubmit(e)}>
-                            Entrar
-                        </button>
-                    </div>
-                </form>
+                        <div className="mt-11 mx-24">
+                            <button className="bg-secundaria text-white px-3 appearance-none block min-w-full py-4 leading-tight rounded-full transition ease-in-out delay-150 bg-secundaria-500 hover:-translate-y-1 hover:scale-100 hover:bg-primaria duration-300"
+                                onClick={(e) => handleSubmit(e)}>
+                                Entrar
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
 
-        <div
-            className="hidden sm:block relative flex-1 p-4 bg-cover bg-[url('capa.jpg')]"
-        ></div>
-    </div>
+            <div
+                className="hidden sm:block relative flex-1 p-4 bg-cover bg-[url('capa.jpg')]"
+            ></div>
+        </div>
     );
 }
 
