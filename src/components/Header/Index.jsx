9eb76ec8ca78144585cd07a64d7b230950a82d93
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-22 15:15:18 -0300

    Correção: Corrigi tamanho da logo para ficar responsivo

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="mx-auto w-full max-w-sm">
             <div className="flex justify-center h-auto mx-auto ">
-                <img src="logo.png" alt="" className="h-28 mt-14 bottom-10" />
+                <img src="logo.png" alt="" className="h-20 mt-14 bottom-10" />
             </div>
         </div>
     );
