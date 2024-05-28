0cda24219e9d875e5e955251e11b224a85d0b600
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-28 16:13:44 -0300

    Recurso: Cria funcionalidade de mostrar boleto

--- src/pages/boleto/Index.jsx
+++ src/pages/boleto/Index.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 
-function Boleto(){
-    return (
-        <div><h1>jadjasjk</h1></div>
-    );
+function Boleto() {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = 'Bearer ' + localStorage.getItem('token');
+      const user = localStorage.getItem('user');
+      const cnp = user.cnp;
+      const autoId = localStorage.getItem('autoId');
+
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://192.168.0.159:80/api/boleto/criar',
+        headers: { 
+          'Content-Type': 'application/x-www-form-urlencoded', 
+          'Accept-Encoding': 'application/json', 
+          'Authorization': token
+        },
+        data: qs.stringify({
+          'autoId': autoId,
+          'cnp': cnp
+        })
+      };
+
+      try {
+        const response = await axios.request(config);
+        setResponse(response.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {response ? (
+        <div dangerouslySetInnerHTML={{ __html: response }} />
+      ) : (
+        <div>{error ? <p>Error: {error}</p> : <p>Loading...</p>}</div>
+      )}
+    </div>
+  );
 }
 
 export default Boleto;
