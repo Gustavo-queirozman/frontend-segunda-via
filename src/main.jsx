951dee0988f685d26bb3596d326702a9971812d0
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2024-05-29 16:06:25 -0300

    Recurso: Cria funcionalidade de mostrar documento

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import EsqueciMinhaSenha from './pages/EsqueciMinhaSenha/Index.jsx';
 import ModificarSenha from './pages/ModificarSenha/Index.jsx';
 import Boletos from './pages/boletos/Index.jsx';
 import Boleto from './pages/boleto/Index.jsx';
+import Documento from './pages/Documento/Index.jsx';
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
   {
     path: "/boleto",
     element: <Boleto/>
+  },
+  {
+    path: "/documento",
+    element: <Documento/>
   }
 ]);
 
